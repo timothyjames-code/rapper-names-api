79199e4540167f2f307fbd581712a31bf3ae7b93
Tim <Ti.corwin229@gmail.com>
2023-09-30 11:59:25 -0400

    first commit to rappers api build

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+  '21 Savage': {
+  'age': 29,
+  'birthName': "Sheyaa Bin Abraham-Joseph",
+  'birthLocation': 'London, England'
+},
+
+'Chance the Rapper':{
+  'age': 29,
+  'birthName': 'Chancelor Bennett',
+  'birthLocation': 'Chicago, Illinois'
+},
+'unknown':{
+  'age': 0,
+  'birthName': 'unknown',
+  'birthLocation': 'unknown'
+}
+}
+
+app.get('/', (request, response)=> {
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+  const rapperName = request.params.name.toLowerCase()
+  if( rappers[rapperName]){
+    response.json(rappers[rapperName])
+  }else{
+    response.json(rappers['unknown'])
+  }
+})
+
+app.listen(PORT, ()=>{
+  console.log(`The server is now running on port ${PORT}!`)
+})
