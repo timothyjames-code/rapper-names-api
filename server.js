28940a7b9b2d5ca050d88ba417fd5ae325008c74
Tim <Ti.corwin229@gmail.com>
2023-09-30 17:38:18 -0400

    added heroku port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/api/:name', (request, response)=>{
   }
 })
 
-app.listen(process,env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
   console.log(`The server is now running on port ${PORT}!`)
 })
